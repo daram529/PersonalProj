21e5ed006733c74af262a43b1ba2d017e9fa8e79
YUN <ymk1211@kaist.ac.kr>
2017-03-13 02:07:40 +0900

    front

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var http = require('http');
 var fs = require('fs');
-var jade = require('jade');
 
 http.createServer(function(request, response) {
-  fs.readFile('test.jade', 'utf-8', function(err, data) {
-    var fn = jade.compile(data);
-    response.writeHead(200, {'ContentType':'text.html'});
-    response.end(fn());
+  fs.readFile('index.html', 'utf-8', function(err, data) {
+    response.writeHead(200, {'ContentType':'text/html'});
+    response.end(data);
   });
 }).listen(8080, function() {
   console.log('Server Running....http://192.168.0.8:8080');
